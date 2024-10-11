f779d3533b70cf60c7e6ffba78113342ddc08688
JuanJhonv <valdiviesofernandezj@gmail.com>
2024-10-11 18:56:12 -0500

    Avance modulo produccion

--- Produccion/Routers/produccion.js
+++ Produccion/Routers/produccion.js
@@ -3,7 +3,7 @@ import { ProduccionController } from "../Controladores/produccion.js";
 
 export const produccionRouter = Router()
 produccionRouter.get('/',ProduccionController.getOrdenes)
-produccionRouter.get('/:id',ProduccionController.getOrdenes)
+produccionRouter.get('/:id',ProduccionController.getOrdenesById)
 produccionRouter.post('/',ProduccionController.pushItems)
 produccionRouter.put('/:id/:data',ProduccionController.updateItems)
 produccionRouter.delete('/:id',ProduccionController.deleteOrden)
