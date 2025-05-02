c1e29bcc43a98385d5e7b6a34cd93f9bf0791e7b
juanpablov <valdiviesofernandezj@gmail.com>
2025-05-02 00:47:06 -0500

    Correcion modulo prestamos

--- Main/config.js
+++ Main/config.js
@@ -2,4 +2,4 @@ export const PORT_DEFAULT = 4000
 export const SECRET_JWT_KEY = 'info_fdasdf_234%%$_privda_1988128__sdf'
 export const SECRET_JWT_KEY2 = 'info_fdasdf_234%%$_privda_1988128__qqq'
 // export const ORIGINS = ['http://localhost:5173','http://192.168.18.20:5173']
-export const ORIGINS = ['http://localhost:5173', 'http://192.168.0.171:5173', 'http://192.168.1.104:5173', 'http://192.168.43.242:5173', 'http://192.168.18.20:5173']
+export const ORIGINS = ['http://localhost:5173', 'http://192.168.0.171:5173', 'http://192.168.137.1:5173', 'http://192.168.43.242:5173', 'http://192.168.18.20:5173']
